15f2e2691b552c60c437199c6b67c3b9cc0c85bc
Amrutha Varshini Mothajigari <amrutham1802@gmail.com>
2023-09-18 16:40:48 +0530

    promises exercises done

--- day-3.js
+++ day-3.js
@@ -106,3 +106,263 @@ function processData(inputArray) {
     //In blank write anonymous function, which takes one parameter and returns true if its even or false if its odd.
     console.log(myFilter(inputArray,function (ele){return ele%2==0?true:false}));   
 } 
+
+
+// 1. Briefly explain different Promise states.
+// Promises can have three different states:
+// Pending: When a promise is initially created, it is in a pending state. This means that the asynchronous operation associated with the promise has not yet completed or failed. 
+// Fulfilled: When the asynchronous operation t represents is successfully completed. This typically means that the desired result or value is now available, and the promise can deliver this value to any registered .then() handlers.
+// Rejected: If an error occurs during the execution of the asynchronous operation, the promise transitions to the rejected state. In this state, the promise contains information about the error, and it can pass this error to any registered .catch() or .then() handlers with an error callback.
+// 2. Using promise to create a delayedLowerCase function which will transform a string to lower case after 5 seconds. Ex Input: ""BEAUTIFUL CODE"" Output: ""beautiful code""
+function delayedLowerCase(input) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (typeof input === 'string') {
+          resolve(input.toLowerCase());
+        } else {
+          reject(new Error('Input is not a string'));
+        }
+      }, 5000); 
+    });
+  }
+  
+  // Calling the function
+  delayedLowerCase("BEAUTIFUL CODE")
+    .then(result => {
+      console.log(result); 
+    })
+    .catch(error => {
+      console.error(error);
+    });
+
+// 3. Briefly explain Promise Chaining. For the above created delayedLowerCase function chain a promise which will print the string and its length: Ex: Input: ""Beautiful Code""  Output: ""beautiful code - 14"""
+//Promise chaining is a technique in JavaScript that allows you to sequentially execute multiple asynchronous operations, one after the other, using promises. It involves using the .then() method to attach additional actions to a promise's resolution, effectively creating a chain of asynchronous tasks.
+const my_promise=function delayedLowerCase(input) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof input === 'string') {
+        resolve(input.toLowerCase());
+      } else {
+        reject(new Error('Input is not a string'));
+      }
+    }, 5000); 
+  });
+}
+
+my_promise("BEAUTIFUL CODE")
+  .then(result => {
+    return result;
+  })
+  .then(result => {
+    console.log(result+'-'+result.length); 
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+  
+my_promise("BEAUTIFUL CODE")
+  .then(result => {
+    new Promise((resolve,reject)=>{
+      setTimeout(()=>{console.log(result+'-'+result.length)},1000);
+    });
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+// 1. Rewrite the below using Async-Await syntax
+// ```
+// function getProcessedData(url) {
+//   return downloadData(url) // returns a promise
+//     .catch(e => {
+//       return downloadFallbackData(url)  // returns a promise
+//     })
+//     .then(v => {
+//       return processDataInWorker(v)  // returns a promise
+//     })
+// }
+async function getProcessedData(url) {
+try {
+    data = await downloadData(url);
+  } catch (e) {
+    data = await downloadFallbackData(url);
+  }
+  return processDataInWorker(data);
+}
+
+//
+///
+//function to display lowercase of a string
+function delayedLowerCase(input)
+{
+  setTimeout(()=>{
+    console.log(input.toLowerCase());
+  },5000);
+}
+delayedLowerCase('BEAUTIFUL CODE');
+
+//using callback
+function lowercase(input)
+{
+  console.log(input.toLowerCase());
+}
+function delayedLowerCase(input,callback)
+{
+  setTimeout(()=>callback(input),5000);
+}
+delayedLowerCase('BEAUTIFUL CODE',lowercase);
+
+//using promises
+function delayedLowerCase(input) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof input === 'string') {
+        console.log(input.toLowerCase())
+        resolve(input);
+      } else {
+        reject(new Error('Input is not a string'));
+      }
+    }, 5000); 
+  });
+}
+
+// Calling the function
+delayedLowerCase("BEAUTIFUL CODE");
+
+//using async and await
+function toLowerCase(input) {
+  return input.toLowerCase();
+}
+async function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function delayedLowerCase(input) {
+  await wait(5000); // Wait for 10 seconds
+  const result = toLowerCase(input);
+  console.log(result);
+}
+// Calling the function
+delayedLowerCase('BeautifulCODE');
+
+
+//Process Data function using callbacks,Promises and async-await
+//ProcessingData function for fetching , transforming and processing the data
+function fetchData() {
+    console.log('In fetchData function')
+    return "Hello, ";
+  }
+  
+  function transformData(data) {
+    console.log("In transformData function")
+    return data + "World!";
+  }
+  
+  function processData() {
+    console.log("Performing processData function")
+    const data = fetchData();
+    const result = transformData(data);
+    return result;
+  }
+  
+  const result = processData();
+  console.log(result); // "Hello, World!"
+
+  //Process Data function using Callbacks
+
+  function fetchData(callback) {
+    setTimeout(() => {
+      console.log('In fetchData function');
+      const data = "Hello, ";
+      callback(data);
+    }, 1000);
+  }
+  
+  function transformData(data, callback) {
+    setTimeout(() => {
+      console.log("In transformData function");
+      const result = data + "World!";
+      callback(result);
+    }, 1000);
+  }
+  
+  function processDataUsingCallbacks(callback) {
+    console.log("Performing processData function");
+    fetchData(data => {
+      transformData(data, result => {
+        callback(result);
+      });
+    });
+  }
+  
+  processDataUsingCallbacks(result => {
+    console.log(result); 
+  });
+  
+  //Process Data function using Promises
+  function fetchData() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log('In fetchData function');
+        const data = "Hello, ";
+        resolve(data);
+      }, 1000);
+    });
+  }
+  
+  function transformData(data) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        console.log("In transformData function");
+        const result = data + "World!";
+        resolve(result);
+      }, 1000);
+    });
+  }
+  
+  function processDataUsingPromises() {
+    console.log("Performing processData function");
+    return fetchData()
+      .then(data => transformData(data))
+      .then(result => result);
+  }
+  
+  processDataUsingPromises().then(result => {
+    console.log(result); 
+  });
+  
+  //using await and async functions
+  async function fetchDataAsync() {
+    console.log('In fetchData function');
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const data = "Hello, ";
+        resolve(data);
+      }, 1000);
+    });
+  }
+  
+  async function transformDataAsync(data) {
+    console.log("In transformData function");
+    return new Promise(resolve => {
+      setTimeout(() => {
+        const result = data + "World!";
+        resolve(result);
+      }, 1000);
+    });
+  }
+  
+  async function processDataUsingAsyncAwait() {
+    console.log("Performing processData function");
+    const data = await fetchDataAsync();
+    const result = await transformDataAsync(data);
+    return result;
+  }
+  
+  processDataUsingAsyncAwait().then(result => {
+    console.log(result); 
+  });
+
+
+  
+  
--- ex.js
+++ ex.js
@@ -1,20 +1,71 @@
-function download(url, callback) {
+ function delayedLowerCase(input) {
+  return new Promise((resolve, reject) => {
     setTimeout(() => {
-        // script to download the picture here
-        console.log(`Downloading ${url} ...`);
-        // process the picture once it is completed
-        callback(url);
-
-    }, 0);
+      if (typeof input === 'string') {
+        resolve(input.toLowerCase());
+      } else {
+        reject(new Error('Input is not a string'));
+      }
+    }, 5000); 
+  });
 }
-console.log("hiii");
-console.log("hiii");
-let url = 'https://www.javascripttutorial.net/pic.jpg';
-download(url, function(picture) {
-    console.log(`Processing ${picture}`);
-}); 
-console.log("hiii");
-console.log("hiii");
-console.log("hiii");
-console.log("hiii");
-console.log("hiii");
+//  Calling the function
+delayedLowerCase("BEAUTIFUL CODE")
+  .then(result => {
+    console.log(result); 
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+//function to display lowercase of a string
+function delayedLowerCase(input)
+{
+  setTimeout(()=>{
+    console.log(input.toLowerCase());
+  },5000);
+}
+delayedLowerCase('BEAUTIFUL CODE');
+
+//using callback
+function lowercase(input)
+{
+  console.log(input.toLowerCase());
+}
+function delayedLowerCase(input,callback)
+{
+  setTimeout(()=>callback(input),5000);
+}
+delayedLowerCase('BEAUTIFUL CODE',lowercase);
+
+//using promises
+function delayedLowerCase(input) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (typeof input === 'string') {
+        console.log(input.toLowerCase())
+        resolve(input);
+      } else {
+        reject(new Error('Input is not a string'));
+      }
+    }, 5000); 
+  });
+}
+
+// Calling the function
+delayedLowerCase("BEAUTIFUL CODE");
+
+//using async and await
+function toLowerCase(input) {
+  return input.toLowerCase();
+}
+async function wait(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function delayedLowerCase(input) {
+  await wait(5000); // Wait for 10 seconds
+  const result = toLowerCase(input);
+  console.log(result);
+}
+// Calling the function
+delayedLowerCase('BeautifulCODE');
